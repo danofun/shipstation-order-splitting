59194eaaf6d59de350a1fa5d2634568d647337d8
danofun <danofun@tuta.io>
2022-06-15 13:24:28 -0400

    add react frontend

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const express = require("express");
+const path = require('path');
+const express = require('express');
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, './client/build')));
 
 const shipStationRoutes = require("./routes/shipstation");
 const compression = require("compression");
